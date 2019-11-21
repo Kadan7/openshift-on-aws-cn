c92f8cf9fdc483712dfd7d3d5223c10d793a8782
harold <bnusunny@gmail.com>
2019-11-21 15:25:46 +0800

    final version for v3.10

--- ocp-4/bin/ocp-4.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { Ocp4Stack } from '../lib/ocp-4-stack';
-
-const app = new cdk.App();
-new Ocp4Stack(app, 'Ocp4Stack');
